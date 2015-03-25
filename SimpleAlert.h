d8a50046445ea77ba807c75be30b2cd42a105b68
Luis Portillo Miranda <miranda.luis@gmail.com>
2015-03-25 09:49:04 -0400

    Create SimpleAlert.h

---
+++ SimpleAlert.h
@@ -0,0 +1,14 @@
+//
+//  SimpleAlert.h
+//
+//  Created by Luis Portillo Miranda on 22/1/15.
+//  Copyright (c) 2015 Guaoo Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleAlert : UIAlertView
+
+- (void) title:(NSString *)title message:(NSString *)message cancelButton:(NSString *)cancelButton;
+
+@end
